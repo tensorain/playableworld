382c152379e4b49b7eab4799a3b8c49a26776daa
root <tensorain@gmail.com>
2026-08-09 22:10:43 +0000

    updates

--- create_data.py
+++ create_data.py
@@ -13,3 +13,12 @@ env = gym.make(
 obs, info = env.reset()          # starts an episode; obs = (210,160) uint8 grayscale frame
 
 obs, _, terminated, truncated, info = env.step(action)
+
+def preprocess(obs):
+    play = obs[34:194]   # crop out the scoreboard rows, leaves 160×160 play area
+    return cv2.resize(play, (64, 64), interpolation=cv2.INTER_AREA)  # stays uint8
+
+N = 300_000
+frames  = np.lib.format.open_memmap("data/frames.npy",  mode="w+", dtype=np.uint8, shape=(N, 64, 64))
+actions = np.lib.format.open_memmap("data/actions.npy", mode="w+", dtype=np.uint8, shape=(N,))
+dones   = np.lib.format.open_memmap("data/dones.npy",   mode="w+", dtype=bool,     shape=(N,))
